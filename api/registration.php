94f81abade7308780f9329d3aca24943be077b5a
satvik <satvik.jagannath7@gmail.com>
2016-01-28 00:43:07 +0530

    Registration API : Query ready

--- api/registration.php
+++ api/registration.php
@@ -4,4 +4,11 @@ $response = array(
     "error" => FALSE
 );
 
+$gentoken = md5(uniqid(mt_rand(), true));
+
+if ((!empty($_POST['firstname'])) && (!empty($_POST['lastname'])) && (!empty($_POST['email'])) && (!empty($_POST['encrypted_password'])) && (!empty($_POST['mobilenumber']))) {
+	$sql = "INSERT INTO users (firstname, lastname, email, encrypted_password, mobilenumber, token) VALUES ('" . $_POST['firstname'] . "', '" . $_POST['lastname'] . "', '" . $_POST['email'] . "', '" . hash('sha512', $_POST['encrypted_password']) . "', '" . $_POST['mobilenumber'] . "' , '" . $gentoken . "')";
+	
+}
+
 ?>
